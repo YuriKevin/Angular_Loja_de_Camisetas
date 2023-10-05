9352983520efd657b7bae2223eb5fcd8ba080857
Yuri Kevin <91747141+YuriKevin@users.noreply.github.com>
2023-10-05 15:21:42 -0300

    TEST camiseta.service.ts

--- src/app/modules/camiseta/camiseta.service.ts
+++ src/app/modules/camiseta/camiseta.service.ts
@@ -11,7 +11,7 @@ import { Camiseta } from 'src/app/modules/camiseta/camiseta';
 })
 export class CamisetaService {
 
-  private apiURL = "https://camisetas.up.railway.app/";
+  private apiURL = "http://localhost:8080/";
 
   httpOptions = {
     headers: new HttpHeaders({
